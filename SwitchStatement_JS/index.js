d1385627bf179313d0dee89dbf875f5e302c488e
jaisuvinu <jaisuvinu@gmail.com>
2022-06-30 15:47:23 -0700

    first commit

---
+++ SwitchStatement_JS/index.js
@@ -0,0 +1,20 @@
+let grade ="F";
+switch(grade){
+    case "A":
+        console.log("You did Great!");
+        break;
+    case "B":
+        console.log("You did Good!");
+        break;
+    case "C":
+        console.log("You did Okay!");
+        break;
+    case "D":
+        console.log("You Passed.....Barely");
+        break;
+    case "F":
+        console.log("You Failed!");
+        break;
+    default:
+        console.log(grade," is not a letter");
+}
